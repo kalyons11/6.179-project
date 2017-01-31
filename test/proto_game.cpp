756ecffdd6f819506c935c6be8e43be1aaf56bb7
Will Roberts <Will Roberts>
2017-01-31 00:47:21 -0500

    try to get to run

--- test/proto_game.cpp
+++ test/proto_game.cpp
@@ -1,11 +1,72 @@
 #include <SDL.h>
 #include <iostream>
+#include <stdlib.h>
+#include <random>
+#include <ctime>
+#include <stdbool.h>
+
 using namespace std;
+
+bool init_everything();
+bool init_sdl();
+bool create_window();
+bool create_renderer();
+void set_up_renderer();
+
+void render();
+void run_game();
+
+void print_scores();
+bool keep_playing();
 
 int posX = 100;
 int posY = 200;
 int sizeX = 600;
 int sizeY = 400;
+
+SDL_Rect pos_player1;
+SDL_Rect pos_player2;
+
+// Kevin.
+
+SDL_Rect ballPos;
+
+mt19937 rng;
+
+uniform_real_distribution<double> x_dist(10, sizeX - 10);
+uniform_real_distribution<double> y_dist(10, sizeY - 10);
+
+int x_speed = 7;
+int y_speed = 3;
+
+bool started = false;
+
+int max_score = 7;
+
+int left_score = 0;
+int right_score = 0;
+
+int game_mode = -1;
+
+void print_scores() {
+	cout << "CURRENT SCORING" << endl;
+	cout << "Player 1 (Left): " << left_score << " | Player 2 (Right): " << right_score << endl;
+	cout << "----------" << endl << endl;
+}
+
+bool go = true;
+
+bool keep_playing() {
+	// Determine if we should keep going or end the game...
+	if (left_score < max_score && right_score < max_score) {
+		return true;
+	} else if ((left_score == max_score && right_score == max_score - 1) || (right_score == max_score && left_score == max_score - 1)) {
+		return true;
+		// Win by 2.
+	} else return false;
+}
+
+// Kevin ends here.
 
 SDL_Window* window;
 SDL_Renderer* renderer;
@@ -33,7 +94,7 @@ bool init_sdl(){
 }
 
 bool create_window(){
-	window = SDL_CreateWindow("KW Pong", posX, posY, sizeY, sizeX);
+	window = SDL_CreateWindow("KW Pong", posX, posY, sizeY, sizeX, 0);
 	if(window == nullptr){
 		cout << "Could not create window:" << SDL_GetError()<< endl;
 		return false;
@@ -63,29 +124,101 @@ void render(){
 	SDL_SetRenderDrawColor(renderer, 0, 0 , 0, 255);
 }
 
-void run_game(){
-	bool loop = true;
-	while(loop){
+void run_game() {
+
+	while ( keep_playing() ) // Change this condition.
+	{
 		SDL_Event event;
-		while(SDL_PollEvent(&event)){
-			if(event.type == SDL)
+
+		while ( SDL_PollEvent( & event ) )
+		{
+			if ( event.type == SDL_QUIT )
+				go = false;
+			else if ( event.type == SDL_KEYDOWN )
+			{
+				switch ( event.key.keysym.sym )
+				{
+					case SDLK_s:
+						started = true;
+						break;
+					case SDLK_RIGHT:
+						//++playerPos.x;
+						break;
+					case SDLK_LEFT:
+						//--playerPos.x;
+						break;
+						// Remeber 0,0 in SDL is left-top. So when the user pressus down, the y need to increase
+					case SDLK_DOWN:
+						//++playerPos.y;
+						break;
+					case SDLK_UP:
+						//--playerPos.y;
+						break;
+					default:
+						break;
+				}
+			}
+		}
+
+		if (started) {
+			// Let's move the ball...
+			ballPos.x += x_speed;
+			ballPos.y += y_speed;
+			if (ballPos.x <= 10 || ballPos.x >= sizeX - 10) {
+				// Hit left wall.
+				x_speed = -1 * x_speed;
+			} else if (ballPos.y <= 10 || ballPos.y >= sizeY - 10) {
+				// Hit left wall.
+				y_speed = -1 * y_speed;
+			}
 		}
 	}
 }
 
-SDL_Rect pos_player1;
-SDL_Rect pos_player2;
+
+// Kevin.
+
+
+
+
+
+
+
+
 
 int main( int argc, char* args[]){
-	pos_player1.x = 10
-	pos_player1.y = 200
-	pos_player1.w = 20
-	pos_player1.h = 50
+	pos_player1.x = 10;
+	pos_player1.y = 200;
+	pos_player1.w = 20;
+	pos_player1.h = 50;
 
-	pos_player2.x = 590
-	pos_player2.y = 200
-	pos_player2.w = 20
-	pos_player2.h = 50
+	pos_player2.x = 590;
+	pos_player2.y = 200;
+	pos_player2.w = 20;
+	pos_player2.h = 50;
 
+	ballPos.x = int(x_dist(rng));
+	ballPos.y = int(y_dist(rng));
+	ballPos.w = 20;
+	ballPos.h = 20;
+
+	cout << "Welcome to KW Pong! Select your mode. Type 2 for multiplayer, or 1 for computer." << endl;
+
+	/*cin >> game_mode;
+
+	while (game_mode != 2 && game_mode != 1) {
+		cout << "You must enter a valid game mode! Please try again...";
+		cin >> game_mode;
+	}
+
+	switch (game_mode) {
+		case 1:
+			cout << "Now playing against the computer! Press S to begin. Good luck!" << endl;
+			break;
+		case 2:
+			cout << "Player 1 (left) - use A for UP and Z for down. Player 2 (right) - use UP and DOWN keys." << endl;
+			cout << "Press S to begin. Good luck!" << endl << endl;
+			print_scores();
+	}*/
 	run_game();
 }
